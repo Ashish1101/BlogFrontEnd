cecb97812738a4d53333910eec6638426d65059f
Ashish <ashishskkumar321@gmail.com>
2020-11-03 14:36:12 +0530

    frontEnd

---
+++ build/precache-manifest.9821b62246e2949c712feb78c3099efd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3b5de68fbdbb393ebe1fa7e137763c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "90a75a981bc2c7ed6d0e",
+    "url": "./static/css/2.9aae468c.chunk.css"
+  },
+  {
+    "revision": "e900a560eee7d3233aca",
+    "url": "./static/css/main.ba7a5d97.chunk.css"
+  },
+  {
+    "revision": "90a75a981bc2c7ed6d0e",
+    "url": "./static/js/2.0347b188.chunk.js"
+  },
+  {
+    "revision": "4daeb21b21f9eb29c47908b5f63a9366",
+    "url": "./static/js/2.0347b188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e900a560eee7d3233aca",
+    "url": "./static/js/main.fef6c161.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "f568ba4893f9e49c0444e12e6d25b582",
+    "url": "./static/media/background.f568ba48.png"
+  }
+]);
